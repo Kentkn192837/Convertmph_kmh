368a1e144dc4ee639a358e3dbee7d9b47c4ef414
Kentkn192837 <agriculture5012@gmail.com>
2021-06-22 03:21:46 +0900

    Initial Commit

---
+++ main.js
@@ -0,0 +1,19 @@
+function calc(main)
+{
+    const km = 1;
+    for (let i = km; i <= 190; i++) {
+        main.innerText += `${i}km/h: ${Math.round(i * 0.62 * 100) / 100}mph\n`;
+    }
+}
+
+function draw()
+{
+    document.write("<div id=\"main\"></div>");
+    const main = document.getElementById("main");
+    calc(main);
+}
+
+window.onload = function()
+{
+    draw();
+}
